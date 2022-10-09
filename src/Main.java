0bb3b078d2fefb0b83e3a13d2a8579b3cd859c9b
Cintrian <velegoj2@yandex.ru>
2022-10-09 15:38:01 +0300

    task3

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Задание 1");
-        String firstName = "Ivan";
-        String secondName = "Ivanov";
-        String lastName = "Ivanovich";
+        String firstName = "Семён";
+        String secondName = "Иванов";
+        String lastName = "Семёнович";
+        firstName = firstName.replace("ё", "е");
+        secondName = secondName.replace("ё", "е");
+        lastName = lastName.replace("ё", "е");
         String fullName = secondName + " " + firstName + " " + lastName;
         System.out.println("ФИО сотрудника — " + fullName);
         System.out.println("Задание 2");
-        fullName = fullName.toUpperCase();
+//        fullName = fullName.toUpperCase();
         System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName);
+        System.out.println("Задание 3");
+        System.out.println("Данные ФИО сотрудника — " + fullName);
     }
 }
